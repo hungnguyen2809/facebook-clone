1eb5c70388680a5a4ec088568791b23d0d80b15f
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2020-08-01 23:34:43 +0700

    fix login cua thang Thanh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,18 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: 'index',
-  //   loadChildren: () => import('./pages/index/index.module').then( m => m.IndexPageModule)
-  // },
+  {
+    path: 'index',
+    loadChildren: () => import('./pages/index/index.module').then( m => m.IndexPageModule)
+  },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: 'login',
     pathMatch: 'full'
-  },
-  {
-    path: 'more-actions',
-    loadChildren: () => import('./pages/more-actions/more-actions.module').then( m => m.MoreActionsPageModule)
   }
 ];
 
