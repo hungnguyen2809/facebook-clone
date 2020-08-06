3de7e5826a37a865702e2f6cb08d23c57d35a042
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2020-08-06 23:04:48 +0700

    change image avartar

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -14,35 +14,35 @@ export class NotificationsPage implements OnInit {
   ngOnInit() {
     this.listNotifications = [
       {
-        img: '../../../assets/imgs/avatar.png',
+        img: '../../../assets/imgs/Teacher.png',
         content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga natus dicta voluptas in distinctio, veritatis ullam quod consequuntur Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga natus dicta voluptas in distinctio, veritatis ullam quod consequuntur',
         type: 1,
         time: 30,
         status: false
       },
       {
-        img: '../../../assets/imgs/avatar.png',
+        img: '../../../assets/imgs/Teacher.png',
         content: 'Hoàng đã bình luận về ảnh của bạn',
         type: 2,
         time: 100,
         status: true
       },
       {
-        img: '../../../assets/imgs/avatar.png',
+        img: '../../../assets/imgs/Teacher.png',
         content: 'Long đã thích ảnh của bạn',
         type: 3,
         time: 4000,
         status: true
       },
       {
-        img: '../../../assets/imgs/avatar.png',
+        img: '../../../assets/imgs/Teacher.png',
         content: 'Nguyễn Tiến Thành và Nguyễn Văn Tráng đã có bồ, 2 bạn đều học chung cùng lớp và tiến tới sáng năm.',
         type: 4,
         time: 120,
         status: false
       },
       {
-        img: '../../../assets/imgs/avatar.png',
+        img: '../../../assets/imgs/Teacher.png',
         content: 'Nguyễn Tiến Thành đã đăng trong nhóm Hội Những Người Đã Có Người Yêu',
         type: 5,
         time: 10,
